7c288267ed8b6fbd896db4bdf41cb9783b3a2a8c
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-18 12:49:58 -0600

    added initial structure and connection to database and sequelize

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    } 
   },
   {
     sequelize,
